114a048eecb52c1283417722ff3b228c7db95770
ninjachen0606 <76736023+ninjachen0606@users.noreply.github.com>
2021-02-12 17:07:25 +0800

    Add files via upload
    Reference: https://www.youtube.com/watch?v=fHtF7BQLlAU&list=PL5PR3UyfTWvfgx9W8WJ9orQf6N1tx0oxN&index=1

---
+++ NavigationSwiftUI/NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by 宇宣 Chen on 2021/2/12.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
